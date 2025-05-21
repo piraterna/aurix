ad1b8406375b1855ff9d0a8eab44a0375e062e2b
Jozef Nagy <creoz@disroot.org>
2025-05-21 22:16:58 +0200

    Updated driver loading template

--- boot/platform/uefi/driver.c
+++ boot/platform/uefi/driver.c
@@ -60,19 +60,19 @@ void load_drivers()
 	// char *driver_binary;
 	// debug("load_drivers(): Loading '%s'...\n", driver_name);
 	//
-	// size_t driver_size = vfs_read(audio_path, &driver_binary);
+	// size_t driver_size = vfs_read(driver_path, &driver_binary);
 	//
-	// audio.devpath[0].Header.Length[0] = sizeof(EFI_MEMMAP_DEVICE_PATH);
-    // audio.devpath[0].Header.Length[1] = sizeof(EFI_MEMMAP_DEVICE_PATH) >> 8;
-    // audio.devpath[0].Header.Type = 1;
-    // audio.devpath[0].Header.SubType = 3;
-    // audio.devpath[0].MemoryType = EfiLoaderData;
-    // audio.devpath[0].StartingAddress = (EFI_UINTPTR)driver_binary;
-    // audio.devpath[0].EndingAddress = (EFI_UINTPTR)driver_binary + driver_size;
-    // audio.devpath[1].Header.Length[0] = sizeof(EFI_DEVICE_PATH_PROTOCOL);
-    // audio.devpath[1].Header.Length[1] = sizeof(EFI_DEVICE_PATH_PROTOCOL) >> 8;
-    // audio.devpath[1].Header.Type = 0x7F;
-    // audio.devpath[1].Header.SubType = 0xFF;
+	// driver_devpath[0].Header.Length[0] = sizeof(EFI_MEMMAP_DEVICE_PATH);
+    // driver_devpath[0].Header.Length[1] = sizeof(EFI_MEMMAP_DEVICE_PATH) >> 8;
+    // driver_devpath[0].Header.Type = 1;
+    // driver_devpath[0].Header.SubType = 3;
+    // driver_devpath[0].MemoryType = EfiLoaderData;
+    // driver_devpath[0].StartingAddress = (EFI_UINTPTR)driver_binary;
+    // driver_devpath[0].EndingAddress = (EFI_UINTPTR)driver_binary + driver_size;
+    // driver_devpath[1].Header.Length[0] = sizeof(EFI_DEVICE_PATH_PROTOCOL);
+    // driver_devpath[1].Header.Length[1] = sizeof(EFI_DEVICE_PATH_PROTOCOL) >> 8;
+    // driver_devpath[1].Header.Type = 0x7F;
+    // driver_devpath[1].Header.SubType = 0xFF;
 	//
 	// status = gSystemTable->BootServices->LoadImage(EFI_FALSE, gImageHandle, (EFI_DEVICE_PATH_PROTOCOL *)driver_devpath, driver_binary, driver_size, &driver_handle);
 	// if (EFI_ERROR(status)) {
