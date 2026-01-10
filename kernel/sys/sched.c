de160fb61ed2884849522a107d6f8fc7b5240bdb
Kevin Alavik <kevin@alavik.se>
2026-01-10 17:16:35 +0100

    fix: Fixed the header comment for sched.c

--- kernel/sys/sched.c
+++ kernel/sys/sched.c
@@ -1,10 +1,19 @@
 /*********************************************************************************/
-/* Module Name:  sched.c */
-/* Project:      AurixOS                                                         */
+/* Module Name:  sched.h */
+/* Project:      AurixOS */
 /*                                                                               */
-/* Copyright (c) 2024-2025 Jozef Nagy                                            */
+/* Copyright (c) 2024-2025 Jozef Nagy */
 /*                                                                               */
-/* This source is subject to the MIT License.                                    */
+/* This source is subject to the MIT License. */
+/* See License.txt in the root of this repository. */
+/*                                                                               */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE */
+/* SOFTWARE. */
 /*********************************************************************************/
 
 #include <sys/sched.h>
