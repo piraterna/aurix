9612ece9279781103c2b59e1c68a68463f7a5dbc
Kevin Alavik <kevin@alavik.se>
2026-03-22 09:16:40 +0100

    feat: better test mlibc

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	printf("Hello, World from %s!\n", "mlibc");
+	void *a = malloc(sizeof(char));
+	if (!a) {
+		printf("error: Failed to allocate using malloc!\n");
+		return 1;
+	}
+	printf("allocated some memory at %p\n", a);
 	return 0;
 }
